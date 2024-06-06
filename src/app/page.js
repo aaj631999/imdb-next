3fed9dbc7d02198fba4f797fd307866e2995b86c
Abhishek <aaj631999@gmail.com>
2024-06-06 18:47:18 +0530

    Added loading effect

--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,7 @@ export default async function page({searchParams}) {
     `https://api.themoviedb.org/3${
       genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
     }?api_key=${API_KEY}&language=en-US&page=1`,
+    {next : {revalidate : 10000}}
   );
   const data = await res.json();
   if(!res.ok){
