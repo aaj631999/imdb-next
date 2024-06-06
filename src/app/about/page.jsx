c4841ba5c6fa94d8757d68b0df85cad37149a3e9
Abhishek <aaj631999@gmail.com>
2024-06-06 15:25:23 +0530

    Added About Page.

---
+++ src/app/about/page.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+      <p className='text-2xl font-medium text-amber-500'>About</p>
+      <p>IMDb is the world's most popular and authoritative source for movie, TV, 
+         and celebrity content. The IMDb consumer site is the #1 movie website in the world 
+         with a combined web and mobile audience of more than 200 million monthly visitors. 
+         IMDb offers a searchable database of more than 250 million data items including 
+         more than 6 million movies, TV shows, and entertainment programs, and more than
+         10 million cast and crew members. </p>
+      <p>IMDb Pro is the essential resource for entertainment industry professionals.
+         This membership-based service includes comprehensive information and tools that are designed
+         to help entertainment industry professionals achieve success throughout all stages of their career.</p>
+      <p>IMDbPro offers members the following: detailed contact and representation information;
+         tools to manage and showcase their IMDb profile, including the ability to select their primary images
+         and the credits they are best “known for”; 
+         exclusive STARmeter rankings that are determined by page views on IMDb;
+         the IMDbPro app for iPhone and Android; IMDbPro Track, which empowers members using the app to receive
+         personalized entertainment industry news and notifications on the people and film and TV projects they
+         want to follow.</p>
+    </div>
+  )
+}
