2552426bb1db80199894a95ebefcd2519fa3f8a6
Abhishek <aaj631999@gmail.com>
2024-06-07 14:59:56 +0530

    Added Card page

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
+import Card from './card'
 
 export default function Results({results}) {
   return (
-    <div>
+    <div className='w-full sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4'>
         {
-            results.map((results)=>(
-                <div key={results.id}>
-                <h2>{results.original_title}</h2>
-                </div>
-            ))
+            results.map((result)=>(
+                <Card key={result.id} result={result}/>
+            ))          
         }
     </div>
   )
